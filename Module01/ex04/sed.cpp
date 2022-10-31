21d1df670141b4e2e27c569858c101b2d7faf7b3
Hssain Aitkadir <haitkadi@e2r11p1.1337.ma>
2022-10-31 19:19:12 +0100

    ex04 Completed, some opemization nedded

--- Module01/ex04/sed.cpp
+++ Module01/ex04/sed.cpp
@@ -1,6 +1,6 @@
 #include "sed.hpp"
 
-std::string *readFileContent(std::string &fileName){
+void    readFileContent(std::string &fileName, std::string &fileContent){
     std::ifstream    infile(fileName);
     std::stringstream buffer;
 
@@ -10,45 +10,32 @@ std::string *readFileContent(std::string &fileName){
     }
     buffer << infile.rdbuf();
     infile.close();
-    std::string *tmp = new(std::nothrow) std::string;
-    if (!tmp)
-        exit(EXIT_FAILURE);
-    *tmp = buffer.str();
-    return (tmp);
+    fileContent = buffer.str();
 }
 
 void    iterFileContent(std::string &fileContent, std::ofstream &outfile, const std::string &oldK, const std::string &newK){
     size_t pos0;
     std::stringstream   tmp;
 
-    while((pos0 = fileContent.find(oldK)) != std::string::npos){
+    while((pos0 = fileContent.find(oldK)) != std::string::npos && !oldK.empty()){
         tmp << fileContent.substr(0, pos0);
         tmp << newK;
         outfile << tmp.str();
-        tmp.clear();
-        
+        tmp.str("");
         fileContent.erase(0, pos0 + oldK.length());
     }
     if (!fileContent.empty()){
         outfile << fileContent;
     }
-    std::cout << std::endl;
-    std::cout << fileContent << std::endl;
-    std::cout << std::endl;
 }
 
 
 void searchAndReplace(std::string fileName, std::string oldKeyword, std::string newKeyword){
- 
-    std::string *fileContent;
+    std::string fileContent;
     std::string outfileName = fileName + ".replace";
-    fileContent = readFileContent(fileName);
-    std::ofstream outfile(outfileName, std::ios_base::app);
-    iterFileContent(*fileContent, outfile, oldKeyword, newKeyword);
-    delete fileContent;
+    readFileContent(fileName, fileContent);
+    std::ofstream outfile(outfileName, std::iostream::trunc);
+    iterFileContent(fileContent, outfile, oldKeyword, newKeyword);
     outfile.close();
-    // std::cout << *fileContent << std::endl;
-    std::cout << oldKeyword << std::endl;
-    std::cout << newKeyword << std::endl;
 
 }
